d82ebbc9665ee9120533571bc81763469839cf56
seyiroks <41244522+seyiroks@users.noreply.github.com>
2025-09-18 21:46:10 +0000

    Add files via upload

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2); // Get arguments passed to script
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
